ccf7bf35a0cf5ff7004a7a0fdebfff9ec94badc8
alleaalexxandra <ghimbasanalexandra@yahoo.com>
2025-11-12 14:00:00 +0200

    feat: add TasksSection component to manage task-related content

---
+++ src/components/Tasks/TaskSection.tsx
@@ -0,0 +1,12 @@
+export function TasksSection() {
+  return (
+    <div className="flex h-full relative">
+      {/* Main Content */}
+      <main className="flex-1 overflow-y-auto flex flex-col">
+        <div className="p-4 md:p-6 w-full flex-1 flex flex-col">
+          <div className="flex-1"></div>
+        </div>
+      </main>
+    </div>
+  );
+}
